68ad5559aebaec85b1c3632360b471f38dbc3619
Marlon Casagranda <marlondiego407@gmail.com>
2015-08-09 22:45:10 -0400

    change ip and poport

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-var express  = require('express');
-
-var app      = express();
-var port  	 = process.env.PORT || 5000;
-var ip			 = process.env.ip || '0.0.0.0';
+var express  = require('express'),
+		app      = express();
 
 app.use(express.static('./Public/VehicleFleet'));
 
-app.listen(port, ip);
-console.log("App listening on port " + port);
+app.listen(process.env.PORT, process.env.ip);
+console.log("App listening on port ");
