2d5fb3fce3664e34a5dc198fbac6f100f21a179a
Marlon Casagranda <marlondiego407@gmail.com>
2015-08-10 00:38:53 -0400

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express  = require('express');
 
 var app      = express();
 var port  	 = process.env.PORT || 5000;
-var port  	 = process.env.ip;
+var ip  	 = process.env.ip;
 
 app.use(express.static('./Public/VehicleFleet'));
 
